7f0a78d3dd77b6c7db8e6c3893892f1fb3510c1a
Naverhound <39379474+Naverhound@users.noreply.github.com>
2020-03-02 20:46:45 -0700

    random
    para evitar fallos

--- user.php
+++ user.php
@@ -20,7 +20,7 @@
 <head>
   <meta charset="utf-8" />
   <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png">
-  <link rel="icon" type="image/png" href="./assets/img/favicon.png">
+  <link rel="icon" type="image/png" href="./assets/img/default-avatar.png">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
   <title>
     CurseTopia
@@ -42,10 +42,11 @@
     <div class="main-panel" id="main-panel">
     <?php include'./inc/views/nav.php'?>
 
-      <div class="content">
+      <div class="content bg-dark ">
+              <h3 >Cursos Adquiridos</h3>
             
+           
       </div>
-     
       <?php include'./inc/views/footer.php'?>
     </div>
   </div>
